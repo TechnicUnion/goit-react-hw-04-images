2418c3f7494a41c1273b6f60aae074af91ad1bcc
TechnicUnion <103375230+TechnicUnion@users.noreply.github.com>
2023-01-02 19:38:55 +0200

    change App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,75 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import { animateScroll as scroll } from 'react-scroll';
 
-class App extends Component {
-  state = {
-    searchQuery: '',
-    page: 1,
-    newFetch: false,
-  };
+export default function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [newFetch, setNewFetch] = useState(false);
 
-  formSubmitHandler = data => {
-    if (this.state.searchQuery !== data) {
-      this.setState({ searchQuery: data, page: 1, newFetch: true });
+  const formSubmitHandler = data => {
+    if (searchQuery !== data) {
+      setSearchQuery(data);
+      setPage(1);
+      setNewFetch(true);
     }
   };
 
-  loadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1, newFetch: false }));
+  const loadMore = () => {
+    setPage(prevState => prevState + 1);
+    setNewFetch(false);
     scroll.scrollToBottom();
   };
 
-  render() {
-    return (
+  return (
+    <div>
+      <Searchbar onSubmit={formSubmitHandler} />
       <div>
-        <Searchbar onSubmit={this.formSubmitHandler} />
-        <div>
-          <ImageGallery
-            searchQuery={this.state.searchQuery}
-            page={this.state.page}
-            newFetch={this.state.newFetch}
-            onClick={this.loadMore}
-          />
-        </div>
+        <ImageGallery
+          searchQuery={searchQuery}
+          page={page}
+          newFetch={newFetch}
+          onClick={loadMore}
+        />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default App;
+// class App extends Component {
+//   state = {
+//     searchQuery: '',
+//     page: 1,
+//     newFetch: false,
+//   };
+
+//   formSubmitHandler = data => {
+//     if (this.state.searchQuery !== data) {
+//       this.setState({ searchQuery: data, page: 1, newFetch: true });
+//     }
+//   };
+
+//   loadMore = () => {
+//     this.setState(prevState => ({ page: prevState.page + 1, newFetch: false }));
+//     scroll.scrollToBottom();
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <Searchbar onSubmit={this.formSubmitHandler} />
+//         <div>
+//           <ImageGallery
+//             searchQuery={this.state.searchQuery}
+//             page={this.state.page}
+//             newFetch={this.state.newFetch}
+//             onClick={this.loadMore}
+//           />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
