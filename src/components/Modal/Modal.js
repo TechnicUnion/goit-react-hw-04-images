d9460d3f2b945e096afb5611bfd6196e179b1a44
TechnicUnion <103375230+TechnicUnion@users.noreply.github.com>
2023-01-02 19:54:53 +0200

    change Madal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,41 +1,71 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import css from '../styles.module.css';
 
-export default class Modal extends Component {
-  componentDidMount = () => {
-    window.addEventListener('keydown', this.handleKeyDown);
-  };
+export default function Modal({ image, onClose }) {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
 
-  componentWillUnmount = () => {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  };
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  });
 
-  handleKeyDown = e => {
+  const handleKeyDown = e => {
     if (e.code === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  handleBackdropCkick = e => {
+  const handleBackdropCkick = e => {
     if (e.currentTarget === e.target) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    return (
-      <div className={css.overlay} onClick={this.handleBackdropCkick}>
-        <div className={css.modal}>
-          <img
-            src={this.props.image.largeImageURL}
-            alt={this.props.image.tags}
-          />
-        </div>
+  return (
+    <div className={css.overlay} onClick={handleBackdropCkick}>
+      <div className={css.modal}>
+        <img src={image.largeImageURL} alt={image.tags} />
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+// export default class Modal extends Component {
+//   componentDidMount = () => {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   };
+
+//   componentWillUnmount = () => {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   };
+
+//   handleKeyDown = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+
+//   handleBackdropCkick = e => {
+//     if (e.currentTarget === e.target) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     return (
+//       <div className={css.overlay} onClick={this.handleBackdropCkick}>
+//         <div className={css.modal}>
+//           <img
+//             src={this.props.image.largeImageURL}
+//             alt={this.props.image.tags}
+//           />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 Modal.propTypes = {
   image: PropTypes.object.isRequired,
